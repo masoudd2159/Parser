01fab17ac72fb4298385971bc35b5857a7774517
Masoud Dabbaghi <masoudd2159@gmail.com>
2018-09-23 20:00:42 +0330

    write mainActivity and DetailActivity

--- app/src/main/java/com/masoudd2159gmail/parser/DetailActivity.java
+++ app/src/main/java/com/masoudd2159gmail/parser/DetailActivity.java
@@ -1,13 +1,50 @@
 package com.masoudd2159gmail.parser;
 
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+
+    String flowerName = null;
+    String flowerPhoto = null;
+    String flowerInstructions = null;
+
+    TextView textViewName;
+    TextView textViewInstructions;
+    ImageView imageViewPhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        init();
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle != null) {
+            if (bundle.containsKey("FlowerName")) {
+                flowerName = bundle.getString("FlowerName");
+                textViewName.setText(flowerName);
+            }
+            if (bundle.containsKey("FlowerPhoto")) {
+                flowerPhoto = bundle.getString("FlowerPhoto");
+                String fileName = flowerPhoto.substring(0, flowerPhoto.indexOf('.'));
+                int resID = getResources().getIdentifier(getApplicationContext().getPackageName() + ":drawable/" + fileName, null, null);
+                imageViewPhoto.setImageBitmap(BitmapFactory.decodeResource(getResources(), resID));
+            }
+            if (bundle.containsKey("FlowerInstructions")) {
+                flowerInstructions = bundle.getString("FlowerInstructions");
+                textViewInstructions.setText(flowerInstructions);
+            }
+        }
+    }
+
+    private void init() {
+        textViewName = findViewById(R.id.textView_Name);
+        textViewInstructions = findViewById(R.id.textView_Instructions);
+        imageViewPhoto = findViewById(R.id.imageView_Photo);
     }
 }
--- app/src/main/java/com/masoudd2159gmail/parser/MainActivity.java
+++ app/src/main/java/com/masoudd2159gmail/parser/MainActivity.java
@@ -1,13 +1,43 @@
 package com.masoudd2159gmail.parser;
 
+import android.app.ListActivity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends ListActivity {
+
+    ArrayList<Flower> flowers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*FlowerXmlPullParser flowerXmlPullParser = new FlowerXmlPullParser();
+        flowers = flowerXmlPullParser.parseXML(MainActivity.this);*/
+
+        FlowerJDOMParser flowerJDOMParser = new FlowerJDOMParser();
+        flowers = flowerJDOMParser.parseXML(MainActivity.this);
+
+        ArrayAdapter<Flower> adapter = new ArrayAdapter<Flower>(this, android.R.layout.simple_list_item_1, flowers);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        Flower currentFlower = flowers.get(position);
+        Intent intent = new Intent(this, DetailActivity.class);
+
+        intent.putExtra("FlowerName", currentFlower.getName());
+        intent.putExtra("FlowerPhoto", currentFlower.getPhoto());
+        intent.putExtra("FlowerInstructions", currentFlower.getInstructions());
+
+        startActivity(intent);
     }
 }
