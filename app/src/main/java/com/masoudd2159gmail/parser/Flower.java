d5bd173e2ba9ed4a494c94b91d551c64ecee2b12
Masoud Dabbaghi <masoudd2159@gmail.com>
2018-09-23 19:22:00 +0330

    create new class for model Data

---
+++ app/src/main/java/com/masoudd2159gmail/parser/Flower.java
@@ -0,0 +1,64 @@
+package com.masoudd2159gmail.parser;
+
+public class Flower {
+    private int productId;
+    private String category;
+    private String name;
+    private String instructions;
+    private String photo;
+    private double price;
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
+
+    public void setInstructions(String instructions) {
+        this.instructions = instructions;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return name + "\n" + price;
+    }
+}
+
